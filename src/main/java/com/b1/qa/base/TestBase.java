6fb24afd6f91d01a1798b7f93966db1bcc32c6f6
ravisingh001 <ravi.singh@mirketa.com>
2021-01-07 16:47:32 +0530

    New Pom and script update 2

--- src/main/java/com/b1/qa/base/TestBase.java
+++ src/main/java/com/b1/qa/base/TestBase.java
@@ -27,7 +27,7 @@ public class TestBase {
 		try {
 			prop = new Properties();
 			FileInputStream ip = new FileInputStream(
-					"/var/jenkins_home/workspace/Mirketa_WebCheck/src/main/java/com/b1/qa/config/Config.properties");
+					"/var/lib/jenkins/workspace/Mirketa_WebCheck/src/main/java/com/b1/qa/config/Config.properties");
 					
 			prop.load(ip);
 		} catch (FileNotFoundException e) {
