2de2b7357c933b8179cb2f39e5f2ef98c7b63eba
ravisingh001 <ravi.singh@mirketa.com>
2021-01-07 22:10:36 +0530

    New commit for chromedriver run 2

--- src/main/java/com/b1/qa/base/TestBase.java
+++ src/main/java/com/b1/qa/base/TestBase.java
@@ -45,21 +45,24 @@ public class TestBase {
 			System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/workspace/Mirketa_WebCheck/Documents/chromedriver");
 			
 			ChromeOptions options = new ChromeOptions();
-			options.addArguments("window-size=1360,768");
-			DesiredCapabilities cap = DesiredCapabilities.chrome();
-			options.setBinary("C:\\Users\\Rai Sigh\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe");
-			//options.setCapability("chrome.binary", "C:\\Program Files (x86)\\Google\\chrome.exe"); 
-			cap.setCapability(ChromeOptions.CAPABILITY, options); 
-				
-			
-			  options.setPageLoadStrategy(PageLoadStrategy.NONE);
-			 options.addArguments("start-maximized");
-			  options.addArguments("enable-automation");
-			 options.addArguments("--no-sandbox");
-			options.addArguments("--disable-infobars");
-			options.addArguments("--disable-dev-shm-usage");
-				options.addArguments("--disable-browser-side-navigation");
-			options.addArguments("--disable-gpu");
+			/*
+			 * options.addArguments("window-size=1360,768"); DesiredCapabilities cap =
+			 * DesiredCapabilities.chrome(); options.
+			 * setBinary("C:\\Users\\Rai Sigh\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe"
+			 * ); //options.setCapability("chrome.binary",
+			 * "C:\\Program Files (x86)\\Google\\chrome.exe");
+			 * cap.setCapability(ChromeOptions.CAPABILITY, options);
+			 * 
+			 * 
+			 * options.setPageLoadStrategy(PageLoadStrategy.NONE);
+			 * options.addArguments("start-maximized");
+			 * options.addArguments("enable-automation");
+			 * options.addArguments("--no-sandbox");
+			 * options.addArguments("--disable-infobars");
+			 * options.addArguments("--disable-dev-shm-usage");
+			 * options.addArguments("--disable-browser-side-navigation");
+			 * options.addArguments("--disable-gpu");
+			 */
 			 
 	        driver = (WebDriver) new ChromeDriver(options);
 	}
